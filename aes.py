ffdb9d8b3a96efe9aee13ae474ac1960f23738e3
KatWalk <shaih82@gmail.com>
2016-05-25 23:20:28 +0300

    1

---
+++ aes.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+msg = 'abc'
+
+def transposition(msg):
+    '''
+    returns transposed msg
+    '''
+    pass
+    
+K = np.diag(np.ones(8))
+np.random.shuffle(K)
+b_msg = np.asarray([list(format(ord(x), 'b')) for x in msg]).astype('int32')
+b_msg = np.hstack([np.zeros((3,1)),b_msg])
+
+#encrypt
+C = np.dot(b_msg,K)
+#decrypt
+M = np.dot(C,K.T)
+recon_msg = np.packbits(M.astype('int'),axis=1).flatten()
+recon_msg = ''.join(map(lambda x:format(x,'c'),recon_msg))
+
+
